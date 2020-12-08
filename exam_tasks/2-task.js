989733d6659b1ae04784248f56f93bfd0a793ee1
gapiyka <50524296+p-pelmeni@users.noreply.github.com>
2020-12-08 12:06:44 +0200

    com

---
+++ exam_tasks/2-task.js
@@ -0,0 +1,16 @@
+//my WEAK parts::
+
+
+//1-CLOSURE
+
+//2-Chaining
+
+//3-Async
+
+//4-f$ing dynamic type definition(determination)
+
+//5-Lambda expression
+
+//6-Huge ammount of other aspects))
+
+//7-.  .  .
