f7122c12ab05beb438dbc18aa2e37a0f3b93ff22
gapiyka <50524296+p-pelmeni@users.noreply.github.com>
2021-01-09 19:19:19 +0200

    tasks++

--- exam_tasks/tasks.js
+++ exam_tasks/tasks.js
@@ -92,8 +92,36 @@ task3();
 //4-Task: (100) Написать класс который имеет колекцию и метод .add(key, value). Note: добавлять объект в колекцию если задан хотябы или key или value.
 //Solution: 
 const task4 = function (){
-    console.log("Task4:");
+    console.log("Task4:");//code spizzen from Andreys friend 
 
+    class Collection{
+        constructor(){
+            this.data = new Map();//u can use:  weakMap/Set/weakSet
+        };
+    
+        add(key, value){
+            if(!value && !key){
+                throw new Error("Exeption: Not a right input");
+            }
+            else{
+                this.data.set(key,value);//adding elements
+            }
+        }
+        output(){
+            for(let k in this.data){
+                console.log(k + " " + this.data[k]);
+            }
+        }
+        get output(){
+            return this.data;
+        }
+    }
+    
+    const myCollection = new Collection();
+    myCollection.add('name', "Vasya");
+    myCollection.add('age', null);
+    myCollection.add('size');
+    console.log(myCollection.output);
 
     console.log("\n");
 }
@@ -152,35 +180,176 @@ task6();
 
 //7-Task: (85) Напишите функцию shuffle(array), которая перемешивает (переупорядочивает случайным образом) элементы массива.
 //Solution: 
+const task7 = function (){
+    console.log("Task7:"); //code spizzen from stackoverflow
 
+    function shuffle(array) {//Fisher–Yates shuffle 
+        let rand, temp;
+        for (let i = array.length - 1; i > 0; i--) {
+            rand = Math.floor(Math.random() * (i + 1));
+            console.log(rand);
+            temp = array[i];
+            array[i] = array[rand];
+            array[rand] = temp;
+        }
+        return array;
+    }
+
+    function shuffle2(array) {//true FISHER algorithm (but with creating array)(solo-made)
+        function random(min, max){
+            return Math.floor(Math.random() * (max - min) + min)
+        }
+        let rand, newArray = [];
+        while(array.length > 0){
+            rand = random(0, array.length);
+            newArray.push(array[rand]);
+            array.splice(rand, 1);
+        }
+        return newArray;
+    }
+
+    let arr = [1,3,3,7]
+    console.log(shuffle(arr));
+    //console.log(shuffle2(arr));
+
+    console.log("\n");
+}
+task7();
 
 //8-Task: (60) Реализовать функцию возвращающую рандомную строку.
 //Solution: 
+const task8 = function (){
+    console.log("Task8:"); 
+    function randomString(length){
+        if(length == undefined) length = 1;
+        let newString = "";
+        let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        for(let i = 0; i < length; i++){
+            newString += alphabet[Math.floor(Math.random() * alphabet.length)];
+        }
+        return newString;
+        }
+        console.log(`Random string: ${randomString(8)}`);
 
+    console.log("\n");
+}
+task8();
 
 //9-Task: (60) Генератор рандомной буквы
 //Solution: 
+const task9 = function (){
+    console.log("Task9:"); 
+    function randomLatter(){
+    let alphabet = "abcdefghijklmnopqrstuvwxyz";
+    return alphabet[Math.floor(Math.random() * alphabet.length)];
+    }
+    console.log(`Random letter: ${randomLatter()}`);
 
+    console.log("\n");
+}
+task9();
 
 //10-Task: (85) Реализовать метод .map() для объекта
 //Solution: 
+const task10 = function (){
+    console.log("Task10:"); //smells like sh1t, but for me ok))
 
+    const callback = function(arg) {
+        //TODO smthing with arg
+        return arg.toString().toLowerCase();
+      };
+    let obj ={
+        name: "Ignat",
+        age: 69,
+        map: function(func){
+            let newObj = Object.create(obj);
+            for(let k in newObj){
+                if(k != "map")
+                {
+                    newObj[k] = func(newObj[k]);
+                }
+            }
+            return newObj;
+        }
+    };
+    console.log(obj);
+    console.log(obj.map(callback));
+    console.log(obj);
+    console.log("\n");
+}
+task10();
 
 //11-Task: (75) Написать функцию которая переводит Римские цифры в Арабские и наоборот.
 //Solution: 
+const task11 = function (){
+    console.log("Task11:"); //code spizzen from coderoad.ru/48946083   and   overcoder.net/q/91876
 
+    function RomanToArabic(number){
+        const map = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000};
+        return [...number].reduce((saved,letter,i,number) => map[number[i+1]] > map[letter] ? saved-map[letter] : saved+map[letter], 0);
+    }
+    function ArabicToRoman(number) {
+        var map2 = {M:1000,CM:900,D:500,CD:400,C:100,XC:90,L:50,XL:40,X:10,IX:9,V:5,IV:4,I:1},roman = '',i;
+        for (let i in map2 ) {
+          while ( number >= map2[i] ) {
+            roman += i;
+            number -= map2[i];
+          }
+        }
+        return roman;
+      }
+      
+    console.log(RomanToArabic("XIV"));
+    console.log(ArabicToRoman(14));
+
+    console.log("\n");
+}
+task11();
 
 //12-Task: (60) Написать функцию на сравнение массивов
 //Solution: 
+const task12 = function (){
+    console.log("Task12:");
 
+    function equal1(arr1, arr2){
+        return JSON.stringify(arr1) == JSON.stringify(arr2);
+    }
+    function equal2(arr1, arr2){//second variation of compare 
+        return arr1.length === arr2.length && arr1.every((x, index) => typeof x == 'object' ? equal2(x, arr2[index]) : x === arr2[index])
+    }
+    let arr4ik =[1,2,[[2,5],4]], arr5ik = [1,2,[[2,5],4]];
+    console.log(equal1(arr4ik, arr5ik))
+    console.log(equal2(arr4ik, arr5ik))
+    console.log("\n");
+}
+task12();
 
 //13-Task: (80) Модуль: Есть строка с имейлами и другой информацией.(Для упрощения вся инфа разделенна символом: ";"). Написать две функции и потом их скомпозировать! 1-ая функция: берет со строки только имейлы и помещаит их в массив. 2-ая функция: Каждый имейл разбиваеться на две части(имя и домен) и помещаеться в еще два разных массива.
 //Solution: 
+const task13 = function (){
+    console.log("Task13:");
 
+
+    console.log("\n");
+}
+task13();
 
 //14-Task: (60) Создать коллекцию Map и пройтись по всем её методам.
 //Solution: 
+const task14 = function (){
+    console.log("Task14:"); 
 
+
+    console.log("\n");
+}
+task14();
 
 //15-Task: (??) Реверс массива. Note: В одну строку(или не более 20 символов) + без использования .reverse
 //Solution: 
+const task15 = function (){
+    console.log("Task15:");
+
+
+    console.log("\n");
+}
+task15();
